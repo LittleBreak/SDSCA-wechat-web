57ade0e889ce059e80ca11b7a856e6673475f92b
caizongding <caizongding@gmail.com>
2019-11-20 23:03:17 +0800

    add header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import styles from './index.css';
+import styles from './index.less';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <h1 className={styles.title}>山东省干细胞学会</h1>
       {props.children}
     </div>
   );
